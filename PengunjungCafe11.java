c1c4a7968d7197767b5796bff793bf3c3b4cb95f
yayaluft <syanuha66@gmail.com>
2025-12-03 10:18:38 +0700

    Modifikasi Pertanyaan No. 2 Percobaan 4

--- PengunjungCafe11.java
+++ PengunjungCafe11.java
@@ -6,8 +6,8 @@ public class PengunjungCafe11 {
     }
     static void daftarPengunjung (String...namaPengunjung) {
         System.out.println("Daftar Nama Pengunjung: ");
-        for (int i =0; i < namaPengunjung.length; i++) {
-            System.out.println("- "+namaPengunjung[i]);
+        for (String nama : namaPengunjung) {
+            System.out.println("- "+nama);
         }
     }
 }
