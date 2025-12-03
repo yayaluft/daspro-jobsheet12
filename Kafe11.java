1cc5f5312cd48b38800611fd2e8f68ecfc010c1b
yayaluft <syanuha66@gmail.com>
2025-12-03 09:55:25 +0700

    Modifikasi No. 2 Percobaan 1

--- Kafe11.java
+++ Kafe11.java
@@ -3,34 +3,38 @@ import java.util.Scanner;
 public class Kafe11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Menu("Andi", true, "DISKON30");
-        System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
-        int pilihanMenu = sc.nextInt();
-        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
-        int banyakItem = sc.nextInt();
+
+        System.out.println("====MENU RESTO KAFE====");
+        System.out.println("1. Kopi Hitam - Rp 15,000");
+        System.out.println("2. Cappucino - Rp 20,000");
+        System.out.println("3. Latte - Rp 22,000");
+        System.out.println("4. Teh Tarik - Rp 12,000");
+        System.out.println("5. Roti Bakar - Rp 10,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
+        System.out.println("===========================");
+        
+        System.out.println("Silakan pilih menu yang Anda inginkan.");
+        System.out.print("MAsukkan jumlah jenis menu yang akan dipesan: ");
+        int jenisMenu = sc.nextInt();
         sc.nextLine();
         System.out.print("Masukan kode promo: ");
         String promo = sc.nextLine();
 
-        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem, promo);
+        int totalHarga = 0;
+        for (int i = 1; i <= jenisMenu; i++) {
+            System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
+            int pilihanMenu = sc.nextInt();
+            System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+            int banyakItem = sc.nextInt();
+
+            totalHarga += hitungTotalHarga(pilihanMenu, banyakItem, promo);
+        }
 
         System.out.println("Total harga untuk pesanan Anda: Rp" + totalHarga);
     }
 
     public static void Menu(String namePelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namePelanggan + "!");
-
-        if (isMember) {
-            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
-        }
-
-        if (kodePromo == "DISKON50") {
-            System.out.println("Kode promo valid: Diskon 50%");
-        } else if (kodePromo == "DISKON30") {
-            System.out.println("Kode promo valid: Diskon 30%");
-        } else {
-            System.out.println("Kode promo invalid");
-        }
 
         System.out.println("====MENU RESTO KAFE====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
